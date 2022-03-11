b0dc5a702d62caa22cf8663fce4cd9f750dfef54
Abdelhadi mohammed salih <abdelhadims@icloud.com>
2022-03-11 22:24:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gang Wars mod
+
+Custom minecraft mod for our modded community minecraft server.
+
+## what are we using
+
+- jdk 8
+- gradle 4.9
+- forge forge-1.12.2-14.23.5.2854
---
+++ src/main/java/com/los/gangwars/GangWars.java
@@ -0,0 +1,29 @@
+package com.los.gangwars;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = GangWars.MODID, name = GangWars.NAME, version = GangWars.VERSION)
+public class GangWars
+{
+    public static final String MODID = "gangwars";
+    public static final String NAME = "LOS Gang Wars";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        logger.info("Hello world!");
+    }
+}
